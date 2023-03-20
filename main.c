ff172c80aba0b047d75fb7b689133c96bdf7d9ba
Matey Nikolov <mateynnikolov@gmail.com>
2023-03-20 11:33:16 +0100

    optimized

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:04:38 by mnikolov          #+#    #+#             */
-/*   Updated: 2023/03/20 11:17:41 by mnikolov         ###   ########.fr       */
+/*   Updated: 2023/03/20 11:33:00 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,29 @@
 char	**ft_add_map(int fd)
 {
 	char	**map;
-	char	*saver;
+	char	*line;
 	char	*buff;
-	int		rb;
+	int		ret;
 
 	buff = ft_calloc(2, sizeof(char));
 	if (!buff)
 		return (NULL);
-	saver = ft_strdup("");
-	rb = 1;
-	while (rb != 0)
+	line = ft_strdup("");
+	ret = 1;
+	while (ret != 0)
 	{
-		rb = read(fd, buff, 1);
-		if (rb == -1)
+		ret = read(fd, buff, 1);
+		if (ret == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		if (rb != 0)
-			saver = ft_strjoin(saver, buff);
+		if (ret != 0)
+			line = ft_strjoin(line, buff);
 	}
 	// free(buff);
-	map = ft_split(saver, '\n');
-	free(saver);
+	map = ft_split(line, '\n');
+	free(line);
 	return (map);
 }
 
