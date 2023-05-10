f22ee920b58a842bdd4a576c82bd3a5925a2b31d
Matey Nikolov <mateynnikolov@gmail.com>
2023-05-10 09:06:41 +0200

    opt

--- srcs/movement_utils.c
+++ srcs/movement_utils.c
@@ -6,7 +6,7 @@
 /*   By: mnikolov <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 10:11:26 by mnikolov          #+#    #+#             */
-/*   Updated: 2023/05/10 08:18:49 by mnikolov         ###   ########.fr       */
+/*   Updated: 2023/05/10 09:06:01 by mnikolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,28 +47,42 @@ int	can_move(t_map *map, double x, double y)
 @brief Moves the player in the direction of the player's movement.
 @param game The game struct.
 */
-void	move_player(t_game *game)
+void move_player(t_game *game)
 {
-	float	step_x;
-	float	step_y;
-	float	forward;
-    float	sideways;
+	float step_x;
+	float step_y;
+	float forward;
+	float sideways;
 
 	step_x = 0;
 	step_y = 0;
+	forward = 1;
+	sideways = 0;
+
 	// Rotate the player's angle according to the rotdir and rotspeed
 	game->player->angle += game->player->rotdir * game->player->rotspeed;
+
 	// Calculate the movement based on the player's angle and movement direction
 	forward = game->player->move * game->player->movespeed;
-    sideways = game->player->side * 2.5;
+	sideways = game->player->side * 2.5;
+
 	step_x = step_x + (cos(game->player->angle) * forward);
 	step_x = step_x + (cos(game->player->angle + PI / 2) * sideways);
-    step_y = step_y + (sin(game->player->angle) * forward);
-    step_y = step_y + (sin(game->player->angle + PI / 2) * sideways);
-	step_x *= 2; // movespeed
-	step_y *= 2; // movespeed
-	if (can_move(game->map, game->player->x + step_x, game->player->y))
-		game->player->x += step_x / 2; // adjust movespeed
-	if (can_move(game->map, game->player->x, game->player->y + step_y))
-		game->player->y += step_y / 2; // adjust movespeed
+	step_y = step_y + (sin(game->player->angle) * forward);
+	step_y = step_y + (sin(game->player->angle + PI / 2) * sideways);
+
+	// Calculate the perpendicular distance from the player to the wall in both the x and y directions
+	int x2 = floor((game->player->x + step_x) / TILES);
+	int y2 = floor((game->player->y + step_y) / TILES);
+	float dist_x = fabs(((x2 * TILES) - game->player->x - (TILES / 2)) / cos(game->player->angle));
+	float dist_y = fabs(((y2 * TILES) - game->player->y - (TILES / 2)) / sin(game->player->angle));
+
+	// Move the player along the direction with the smaller perpendicular distance to the wall
+	if (dist_x < dist_y) {
+		if (can_move(game->map, game->player->x + step_x, game->player->y))
+			game->player->x += step_x / 2; // adjust movespeed
+	} else {
+		if (can_move(game->map, game->player->x, game->player->y + step_y))
+			game->player->y += step_y / 2; // adjust movespeed
+	}
 }
